4b58717d5073c93245a90f060dd7a3f1a7e4fad5
pedro alma <pedroalma@gmail.com>
2024-02-23 11:58:52 -0300

    novo

---
+++ reactjs/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import './Header.css'
+import C from '../../../src/image/Cartao.png';
+function Header (){
+    return(
+        <div class="Header">
+            <div>
+                 <img src= {C}/>
+                 <h1>Melhores</h1>
+                 <h1>preços</h1>
+            </div>
+            <div>
+                 <img src= {C}/>
+                 <h1>Melhores</h1>
+                 <h1>preços</h1>
+            </div>
+            <div>
+                 <img src= {C}/>
+                 <h1>Melhores</h1>
+                 <h1>preços</h1>
+            </div>
+            <div>
+                 <img src= {C}/>
+                 <h1>Melhores</h1>
+                 <h1>preços</h1>
+            </div>
+        </div>       
+    )
+}
+export default Header
