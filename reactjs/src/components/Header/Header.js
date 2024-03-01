21b94658af153d776f1cdc9a200e0688ef4ae84a
pedroalma <pedroalma554@gmail.com>
2024-03-01 11:57:52 -0300

    novo

--- reactjs/src/components/Header/Header.js
+++ reactjs/src/components/Header/Header.js
@@ -1,29 +1,45 @@
 import './Header.css'
 import C from '../../../src/image/Cartao.png';
-function Header (){
+import S from '../../../src/image/Site.png';
+import P from '../../../src/image/2Pessoas.png';
+import D from '../../../src/image/Dinheiro.png';
+import E from '../../../src/image/Envolepes-personalizado.png';
+import V from '../../../src/image/Cartao de visita.png';
+import B from '../../../src/image/Banner Personalizado.png';
+
+function Header(){
     return(
-        <div class="Header">
-            <div>
+     <>
+        <div id="Header">
+            <div className="body">
                  <img src= {C}/>
                  <h1>Melhores</h1>
                  <h1>preços</h1>
             </div>
-            <div>
-                 <img src= {C}/>
+            <div className="body">
+                 <img src= {S}/>
                  <h1>Melhores</h1>
                  <h1>preços</h1>
             </div>
-            <div>
-                 <img src= {C}/>
+            <div className="body">
+                 <img src= {P}/>
                  <h1>Melhores</h1>
                  <h1>preços</h1>
             </div>
-            <div>
-                 <img src= {C}/>
+            <div className="body">
+                 <img src= {D}/>
                  <h1>Melhores</h1>
                  <h1>preços</h1>
             </div>
-        </div>       
+        </div>
+        <div class="caixa">
+          <h2>os mais</h2>
+          <img src={E}/>
+          <img src={V}/>
+          <img src={B}/>
+          <p><u>mais</u></p>
+        </div>
+     </>          
     )
 }
-export default Header
+export default Header;
